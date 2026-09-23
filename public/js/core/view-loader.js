582d4641e2551d4464edcde416904b494e3614eb
David Arruda <david.viniciusarruda@gmail.com>
2026-09-23 17:12:48 -0300

    feat(auth): implementar tela de login no estilo tablet 3D com ilustracao de oficina mecanica e card flutuante

--- public/js/core/view-loader.js
+++ public/js/core/view-loader.js
@@ -32,7 +32,7 @@ const ViewLoader = {
         }
 
         const fileName = customFileName || `${moduleName}.view.html`;
-        const path = `modules/${moduleName}/${fileName}?v=71.0`;
+        const path = `modules/${moduleName}/${fileName}?v=72.0`;
 
         try {
             let html = this._cache.get(path);
