cb63bb9b0415c9e7ae8f9fe997f44c23408e457c
David Arruda <david.viniciusarruda@gmail.com>
2026-09-23 17:39:33 -0300

    style(auth): transformar tela de login para estética Clean Light executiva mantendo as cores Pantone oficiais

--- public/js/core/view-loader.js
+++ public/js/core/view-loader.js
@@ -32,7 +32,7 @@ const ViewLoader = {
         }
 
         const fileName = customFileName || `${moduleName}.view.html`;
-        const path = `modules/${moduleName}/${fileName}?v=76.0`;
+        const path = `modules/${moduleName}/${fileName}?v=77.0`;
 
         try {
             let html = this._cache.get(path);
