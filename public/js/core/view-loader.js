126bf292365cc1f387ee27c73d8877c95c18d8ff
David Arruda <david.viniciusarruda@gmail.com>
2026-09-23 17:58:10 -0300

    feat(auth): intensificar iluminacao e contraste do fundo com orbes amplos e linhas de vento visiveis

--- public/js/core/view-loader.js
+++ public/js/core/view-loader.js
@@ -32,7 +32,7 @@ const ViewLoader = {
         }
 
         const fileName = customFileName || `${moduleName}.view.html`;
-        const path = `modules/${moduleName}/${fileName}?v=80.0`;
+        const path = `modules/${moduleName}/${fileName}?v=81.0`;
 
         try {
             let html = this._cache.get(path);
