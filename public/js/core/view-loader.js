f07aba143226ce3f8c1097906116ec0826d5cdf0
David Arruda <david.viniciusarruda@gmail.com>
2026-09-23 17:01:37 -0300

    feat(auth): redesenhar tela de login com identidade criativa de oficina mecanica, carros e botao engine start

--- public/js/core/view-loader.js
+++ public/js/core/view-loader.js
@@ -32,7 +32,7 @@ const ViewLoader = {
         }
 
         const fileName = customFileName || `${moduleName}.view.html`;
-        const path = `modules/${moduleName}/${fileName}?v=69.0`;
+        const path = `modules/${moduleName}/${fileName}?v=70.0`;
 
         try {
             let html = this._cache.get(path);
