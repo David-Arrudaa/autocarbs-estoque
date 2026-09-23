da7fa5efec7be16f8407d36af214c5ede193e7cc
David Arruda <david.viniciusarruda@gmail.com>
2026-09-23 17:51:44 -0300

    feat(auth): aplicar textura tecnica na onda, micro-brilho metalico, shimmer no botao e animacao escalonada

--- public/js/core/view-loader.js
+++ public/js/core/view-loader.js
@@ -32,7 +32,7 @@ const ViewLoader = {
         }
 
         const fileName = customFileName || `${moduleName}.view.html`;
-        const path = `modules/${moduleName}/${fileName}?v=78.0`;
+        const path = `modules/${moduleName}/${fileName}?v=79.0`;
 
         try {
             let html = this._cache.get(path);
