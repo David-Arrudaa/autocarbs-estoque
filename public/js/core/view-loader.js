28598f61c845ccb1653cbbba77319724714a36da
David Arruda <david.viniciusarruda@gmail.com>
2026-09-24 16:03:15 -0300

    style(cotacao): dar alivio de espaco e ajustar tamanho das caixinhas de marca e valor

--- public/js/core/view-loader.js
+++ public/js/core/view-loader.js
@@ -32,7 +32,7 @@ const ViewLoader = {
         }
 
         const fileName = customFileName || `${moduleName}.view.html`;
-        const path = `modules/${moduleName}/${fileName}?v=84.0`;
+        const path = `modules/${moduleName}/${fileName}?v=85.0`;
 
         try {
             let html = this._cache.get(path);
